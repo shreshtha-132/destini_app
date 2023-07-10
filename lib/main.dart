31db958d2101bc4469a7d241f935569d28d3cb69
shreshtha-132 <shreshthakumargupta@gmail.com>
2023-07-10 13:29:16 +0530

    todo 1-8
    TODO 1:-
    added background image to the app background in container using decoration: box decoration property.
    
    TODO 2:-
    created a new class story to have the storytitle and choices.
    
    TODO 3:-
    added the properties to this class.
    
    TODO 4:-
    created a cosntructor for this.
    
    TODO 5:-
    created a new class story_brain.
    and imported story.dart
    
    TODO 7:-
    included the list in the class.
    
    TODO 8:-
    created a getter method getStory() to return the story title.

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ void main() => runApp(Destini());
 class Destini extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
       home: StoryPage(),
     );
@@ -25,6 +26,10 @@ class _StoryPageState extends State<StoryPage> {
     return Scaffold(
       body: Container(
         //TODO: Step 1 - Add background.png to this Container as a background image.
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('images/background.png'), fit: BoxFit.cover)),
+
         padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 15.0),
         constraints: BoxConstraints.expand(),
         child: SafeArea(
@@ -45,12 +50,13 @@ class _StoryPageState extends State<StoryPage> {
               ),
               Expanded(
                 flex: 2,
-                child: FlatButton(
+                child: TextButton(
                   onPressed: () {
                     //Choice 1 made by user.
                     //TODO: Step 18 - Call the nextStory() method from storyBrain and pass the number 1 as the choice made by the user.
                   },
-                  color: Colors.red,
+                  // color: Colors.red,
+                  style: TextButton.styleFrom(backgroundColor: Colors.red),
                   child: Text(
                     //TODO: Step 13 - Use the storyBrain to get the text for choice 1.
                     'Choice 1',
@@ -67,12 +73,13 @@ class _StoryPageState extends State<StoryPage> {
                 flex: 2,
                 //TODO: Step 26 - Use a Flutter Visibility Widget to wrap this FlatButton.
                 //TODO: Step 28 - Set the "visible" property of the Visibility Widget to equal the output from the buttonShouldBeVisible() method in the storyBrain.
-                child: FlatButton(
+                child: TextButton(
                   onPressed: () {
                     //Choice 2 made by user.
                     //TODO: Step 19 - Call the nextStory() method from storyBrain and pass the number 2 as the choice made by the user.
                   },
-                  color: Colors.blue,
+                  // color: Colors.blue,
+                  style: TextButton.styleFrom(backgroundColor: Colors.blue),
                   child: Text(
                     //TODO: Step 14 - Use the storyBrain to get the text for choice 2.
                     'Choice 2',
